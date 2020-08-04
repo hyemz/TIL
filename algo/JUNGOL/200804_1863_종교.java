135fe0c30d26b43cf2445255a2f4c5e4074ccc76
You Know I Mean <h2m0@swu.ac.kr>
2020-08-04 21:20:09 +0900

    서로소 집합을 이용해 같은 종교를 가진 학생의 수 구하기

---
+++ algo/JUNGOL/200804_1863_종교.java
@@ -0,0 +1,38 @@
+package com.ssafy;
+
+import java.util.Scanner;
+
+public class religion {
+	static int n[];
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		n = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			n[i] = i;
+		}
+
+		int M = sc.nextInt();
+		int cnt = N;
+		for (int i = 0; i < M; i++) {
+			int one = find(sc.nextInt());
+			int two = find(sc.nextInt());
+			
+			if (one != two) {
+				n[two] = one;
+				cnt--;
+			} 
+		}
+		System.out.println(cnt);
+	}
+
+	static int find(int a) {
+		if (n[a] == a)
+			return a;
+		return n[a] = find(n[a]);
+	}
+
+}
