679bb84cc65c2b3297cfe8e9d407588cceabf76b
You Know I Mean <h2m0@swu.ac.kr>
2020-08-30 02:13:46 +0900

    단순 print

---
+++ algo/BOJ/200830_10171_고양이.java
@@ -0,0 +1,15 @@
+package com.ssafy.practice;
+
+public class BOJ_10171_고양이 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+
+	}
+
+}
