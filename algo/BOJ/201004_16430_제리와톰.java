857224942940648ec9045b9c14e0c8961e59deb0
You Know I Mean <h2m0@swu.ac.kr>
2020-10-04 22:22:23 +0900

    [수학] 브론즈 5 이렇게 풀어도 되는건가,,?

---
+++ algo/BOJ/201004_16430_제리와톰.java
@@ -0,0 +1,16 @@
+package com.ssafy.practice;
+
+import java.util.Scanner;
+
+public class BOJ_16430_제리와톰 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		System.out.println(B-A + " " + B);
+	}
+
+}
